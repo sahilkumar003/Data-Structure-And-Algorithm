c822ac0a27daa3375da3ede6511b1d53ecc1fb30
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2022-11-15 00:15:47 +0530

    Time: 11 ms (9.45%), Space: 43.3 MB (5.34%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
@@ -1,24 +1,15 @@
 class Solution {
     public boolean haveConflict(String[] s1, String[] s2) {
         
-       int [][]arr = new int[2][2];
+        int a = Integer.parseInt(s1[0].replaceAll("[^0-9]", ""));
+        int b = Integer.parseInt(s1[1].replaceAll("[^0-9]", "")); 
         
-        arr[0][0] = Integer.parseInt(s1[0].replaceAll("[^0-9]", ""));
-        arr[0][1] = Integer.parseInt(s1[1].replaceAll("[^0-9]", "")); 
+        int c = Integer.parseInt(s2[0].replaceAll("[^0-9]", ""));
+        int d = Integer.parseInt(s2[1].replaceAll("[^0-9]", ""));
         
-        arr[1][0] = Integer.parseInt(s2[0].replaceAll("[^0-9]", ""));
-        arr[1][1] = Integer.parseInt(s2[1].replaceAll("[^0-9]", ""));
-        
-        Arrays.sort(arr, (a,b) -> {
-            return a[1] - b[1];
-        });
-        
-        int max = arr[0][1];
-        for(int i=1;i<arr.length;++i){
-            if(arr[i][0]<=max){
-                return true;
-            }
-        }
+       if(b<=d && c<=b || d<=b && a<=d){
+           return true;
+       }
         
         return false;
     }
