bf9a55ffda4d401d61db88d6a467dae967c3c13a
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2022-11-15 00:19:32 +0530

    Time: 1 ms (86.04%), Space: 42.1 MB (21.17%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean haveConflict(String[] s1, String[] s2) {
         
-        int a = Integer.parseInt(s1[0].replaceAll("[^0-9]", ""));
-        int b = Integer.parseInt(s1[1].replaceAll("[^0-9]", "")); 
+//         int a = Integer.parseInt(s1[0].replaceAll("[^0-9]", ""));
+//         int b = Integer.parseInt(s1[1].replaceAll("[^0-9]", "")); 
         
-        int c = Integer.parseInt(s2[0].replaceAll("[^0-9]", ""));
-        int d = Integer.parseInt(s2[1].replaceAll("[^0-9]", ""));
+//         int c = Integer.parseInt(s2[0].replaceAll("[^0-9]", ""));
+//         int d = Integer.parseInt(s2[1].replaceAll("[^0-9]", ""));
         
-       if(b<=d && c<=b){
-           return true;
-       }else if(d<=b && a<=d){
-           return true;
-       }
+//        if(b<=d && c<=b){
+//            return true;
+//        }else if(d<=b && a<=d){
+//            return true;
+//        }
         
-        return false;
+//         return false;
+        
+         return s1[0].compareTo(s2[1]) <= 0 && s2[0].compareTo(s1[1]) <= 0;
     }
 }
