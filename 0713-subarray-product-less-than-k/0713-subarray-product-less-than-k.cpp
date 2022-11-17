4de082b8c97210e2b06da1735bef6b467fd72cd0
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2022-11-17 17:16:15 +0530

    Time: 213 ms (38.86%), Space: 61.4 MB (20.72%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -4,16 +4,20 @@ public:
          int i = 0;
         int j = 0;
         int ans=0;
-        int curr_product = 1;
-        int n = nums.size();
+        int curr_product=1;
+        int n=nums.size();
         while(i<n)
         {
-            curr_product *= nums[i++];
-            while(curr_product>=k && j<i)
+            curr_product*=nums[i];
+            //cout<<curr_product;
+            while(curr_product>=k  && j<=i)
             {
-                curr_product /= nums[j++];
-            }       
-            ans += i-j;
+                curr_product=(curr_product/nums[j]);
+                j++; 
+            }
+            //cout<<curr_product;
+            ans+=(i-j)+1;
+            i++;
         }
         return ans;
     }
