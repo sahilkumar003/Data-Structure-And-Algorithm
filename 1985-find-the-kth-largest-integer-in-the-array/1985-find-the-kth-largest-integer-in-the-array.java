6852a6f2e7c524645a7703a0e6e90320b7adf23e
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2022-11-23 11:09:45 +0530

    Time: 94 ms (60.00%), Space: 64.3 MB (75.00%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
@@ -7,11 +7,8 @@ class Solution {
             else
                 return Integer.compare(s1.length(), s2.length());
         });
-        for(int i=0;i<k;++i){
-            pq.add(arr[i]);
-        }
-
-        for(int i=k;i<arr.length;++i){
+        
+        for(int i=0;i<arr.length;++i){
            pq.add(arr[i]);
             if(pq.size()>k){
                 pq.remove();
