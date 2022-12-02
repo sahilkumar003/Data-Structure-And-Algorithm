c2834b3ceac905308622ca699471a75e25d8d91c
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2022-12-02 23:52:25 +0530

    Time: 45 ms (84.39%), Space: 45 MB (91.92%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,84 +1,42 @@
 class Solution {
      public int countSubstringForK(int[] arr,int k){
-        int i = -1;
-        int j = -1;
-        int count = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
 
-        while(true){
-            while(i<arr.length-1){
-                i++;
-                int c = arr[i];
-                map.put(c,map.getOrDefault(c,0)+1);
-                if(map.size()<=k){
-                    count += i-j;
-                    continue;
-                }else{
-                    break;
-                }
-            }
+        int j = -1;
+        int count = 0;
 
-            if(i==arr.length-1 && map.size()<=k){
-                break;
-            }
-
-            while(j<i){
-                j++;
-                int c = arr[j];
-                if(map.get(c)==1){
-                    map.remove(c);
+        for(int i=0;i<arr.length;++i){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            while(j<i && map.size()>k){
+                ++j;
+                if(map.get(arr[j])==1){
+                    map.remove(arr[j]);
                 }else{
-                    map.put(c,map.get(c)-1);
-                }
-                if(map.size()>k){
-                    continue;
-                }else{
-                    count += i-j;
-                    break;
+                    map.put(arr[j],map.get(arr[j])-1);
                 }
             }
+            count += i-j;
         }
         return count;
     }
 
     public int countSubstringForK2(int []arr,int k){
-        int i = -1;
-        int j = -1;
-        int count = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
 
-        while(true){
-            while(i<arr.length-1){
-                i++;
-                int c = arr[i];
-                map.put(c,map.getOrDefault(c,0)+1);
-                if(map.size()<=k-1){
-                    count += i-j;
-                    continue;
-                }else{
-                    break;
-                }
-            }
+        int j = -1;
+        int count = 0;
 
-            if(i==arr.length-1 && map.size()<=k-1){
-                break;
-            }
-
-            while(j<i){
-                j++;
-                int c = arr[j];
-                if(map.get(c)==1){
-                    map.remove(c);
+        for(int i=0;i<arr.length;++i){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            while(j<i && map.size()>k-1){
+                ++j;
+                if(map.get(arr[j])==1){
+                    map.remove(arr[j]);
                 }else{
-                    map.put(c,map.get(c)-1);
-                }
-                if(map.size()>k-1){
-                    continue;
-                }else{
-                    count += i-j;
-                    break;
+                    map.put(arr[j],map.get(arr[j])-1);
                 }
             }
+            count += i-j;
         }
         return count;
     }
