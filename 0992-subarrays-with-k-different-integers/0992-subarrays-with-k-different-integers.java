495d15bd2d9b4aa2a661d1b65eecec661b9fbc45
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2022-12-04 13:14:33 +0530

    Time: 101 ms (54.16%), Space: 72.6 MB (37.09%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -20,30 +20,30 @@ class Solution {
         return count;
     }
 
-    public int countSubstringForK2(int []arr,int k){
-        HashMap<Integer,Integer> map = new HashMap<>();
+//     public int countSubstringForK2(int []arr,int k){
+//         HashMap<Integer,Integer> map = new HashMap<>();
 
-        int j = -1;
-        int count = 0;
+//         int j = -1;
+//         int count = 0;
 
-        for(int i=0;i<arr.length;++i){
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-            while(j<i && map.size()>k-1){
-                ++j;
-                if(map.get(arr[j])==1){
-                    map.remove(arr[j]);
-                }else{
-                    map.put(arr[j],map.get(arr[j])-1);
-                }
-            }
-            count += i-j;
-        }
-        return count;
-    }
+//         for(int i=0;i<arr.length;++i){
+//             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+//             while(j<i && map.size()>k-1){
+//                 ++j;
+//                 if(map.get(arr[j])==1){
+//                     map.remove(arr[j]);
+//                 }else{
+//                     map.put(arr[j],map.get(arr[j])-1);
+//                 }
+//             }
+//             count += i-j;
+//         }
+//         return count;
+//     }
     
     public int subarraysWithKDistinct(int[] arr, int k) {
     
-        return (countSubstringForK(arr,k)-countSubstringForK2(arr,k));
+        return (countSubstringForK(arr,k)-countSubstringForK(arr,k-1));
 
     }
 }
