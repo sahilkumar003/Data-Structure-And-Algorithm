4067ea6b7892ba9104ad0ed65f96cb932b856d96
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2022-12-21 12:12:16 +0530

    Time: 82 ms (34.03%), Space: 60.8 MB (79.06%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minOperations(int[] arr, int x) {
+        if(arr[0]>x && arr[arr.length-1]>x){
+            return -1;
+        }
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+      map.put(0,-1);
+
+      int sum = 0;
+      int max = 0;
+
+        for (int j : arr) {
+            sum += j;
+        }
+        
+        if(sum<x){
+            return -1;
+        }
+
+      int req = sum - x;
+      int tempSum = 0;
+      
+      for(int i=0;i<arr.length;++i){
+           tempSum += arr[i];
+           int temp = tempSum-req;
+
+          if(map.containsKey(temp)){
+              int length = i-map.get(temp);
+              max =  Math.max(max,length);
+          }
+          map.put(tempSum,i);
+      }
+
+      if(max==Integer.MIN_VALUE && sum!=x){
+          return -1;
+      }else{
+          return arr.length-max;
+      }
+    }
+}
