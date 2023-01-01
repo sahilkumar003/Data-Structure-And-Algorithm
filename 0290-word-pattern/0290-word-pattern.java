4baf5ee60c646ac82d41c3555783e228610b8309
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-01-01 23:36:03 +0530

    Time: 1 ms (95.36%), Space: 40.2 MB (80.47%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -38,13 +38,15 @@ class Solution {
         
         
         
-        String[] words = s.split(" ");
-    if (words.length != pattern.length())
-        return false;
-    Map index = new HashMap();
-    for (Integer i=0; i<words.length; ++i)
-        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+         String[] strSplit = s.split(" ");
+        Map<Object,Object> map = new HashMap<>();
+        
+        if (pattern.length() != strSplit.length) return false;
+        
+        for (Integer i = 0; i < strSplit.length; i++) {
+            if (map.putIfAbsent(pattern.charAt(i),i) != map.putIfAbsent(strSplit[i],i))
             return false;
-    return true;
+        }
+        return true;
     }
 }
