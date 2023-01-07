4cefa67271e9fe7aadf11a96f8de3489027b9fc2
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-01-07 12:35:41 +0530

    Time: 1365 ms (9.55%), Space: 78.2 MB (62.92%) - LeetHub

---
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String addSpaces(String s, int[] arr) {
+        Arrays.sort(arr);
+        
+        StringBuffer str = new StringBuffer(s);
+        
+        int count = 0;
+        
+        for(int i=0;i<arr.length;++i){
+            str.insert(arr[i]+count," ");
+            count++;
+        }
+        
+        return str.toString();
+    }
+}
