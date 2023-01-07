1c743f431797a9e00cad3396876ff93eee44c59f
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-01-07 22:51:54 +0530

    Time: 8 ms (42.89%), Space: 43.1 MB (74.17%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,29 @@
+class Solution {
+      public List<String> permutationSpace(String p, String up, ArrayList<String> list){
+        if(up.length()==0){
+            list.add(p);
+            return list;
+        }
+
+        char c = up.charAt(0);
+
+        if(Character.isDigit(c)){
+            permutationSpace(p+c,up.substring(1),list);
+        }else{
+           if(Character.isUpperCase(c)){
+                permutationSpace(p+c,up.substring(1),list);
+                permutationSpace(p+Character.toLowerCase(c),up.substring(1),list); 
+           }else{
+                permutationSpace(p+c,up.substring(1),list);
+                permutationSpace(p+Character.toUpperCase(c),up.substring(1),list); 
+           }
+        }
+            
+          return list;
+    }
+    
+    public List<String> letterCasePermutation(String s) {
+        
+        return permutationSpace("",s,new ArrayList<>());
+    }
+}
