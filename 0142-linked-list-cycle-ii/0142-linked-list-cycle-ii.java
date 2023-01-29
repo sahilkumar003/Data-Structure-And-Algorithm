829a83849d8d04279cb5b646772df43e94bd7c4a
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-01-29 22:12:18 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (68.64%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,15 +11,34 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashMap<ListNode,Integer> map = new HashMap<>();
-        ListNode temp = head;
+//         HashMap<ListNode,Integer> map = new HashMap<>();
+//         ListNode temp = head;
         
-        while(temp!=null){
-            if(map.containsKey(temp)){
-                return temp;
+//         while(temp!=null){
+//             if(map.containsKey(temp)){
+//                 return temp;
+//             }
+//             map.put(temp,1);
+//             temp = temp.next;
+//         }
+        
+//         return null;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(fast==slow){
+                fast = head;
+                while(fast != slow){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return fast;
             }
-            map.put(temp,1);
-            temp = temp.next;
         }
         
         return null;
