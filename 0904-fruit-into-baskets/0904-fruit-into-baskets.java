e9b8dbb68daf628274c94a43fb942f99657921f2
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-02-07 10:45:57 +0530

    Time: 55 ms (66.95%), Space: 50.8 MB (82.23%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int totalFruit(int[] arr) {
+        if(arr.length==1 || arr.length==2){
+            return arr.length;
+        }
+        
+        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
+        int max = 0;
+        int sum = 0;
+        int j = 0;
+        
+        for(int i=0;i<arr.length;++i){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            
+            if(map.size()>2){
+                sum++;
+                while(map.size()>2){
+                    if(map.get(arr[j])<=1){
+                        map.remove(arr[j]);
+                    }else{
+                        map.put(arr[j],map.get(arr[j])-1);
+                    }
+                    j++;
+                    sum--;
+                }
+            }else{
+                sum += 1;
+                max = Math.max(max,sum);
+            } 
+        }
+        
+        return max;
+    }
+}
