45ea90d1c02ea2913b55704ed893a34b71670052
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-02-08 17:03:12 +0530

    Time: 1 ms (99.45%), Space: 42.6 MB (75.83%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int jump(int[] arr) {
+        if(arr.length==1){
+            return 0;
+        }
+        
+       int current = 0;
+       int maxPossible = 0;
+       int jump = 0; 
+        
+        for(int i=0;i<arr.length-1;++i){
+            maxPossible = Math.max(maxPossible,i+arr[i]);
+            
+            if(i==current){
+                current = maxPossible;
+                jump++;
+            }
+            
+        }
+        
+        return jump;
+    }
+}
