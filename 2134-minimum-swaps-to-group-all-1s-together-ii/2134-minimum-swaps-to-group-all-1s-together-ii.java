44a10493686cea0ffd17c031564f5b87515306e1
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-02-12 23:19:38 +0530

    Time: 16 ms (41.30%), Space: 51 MB (83.40%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
@@ -9,25 +9,23 @@ class Solution {
                 one++;
             }
         }
-    
-        int zero = 0;
-        int min = Integer.MAX_VALUE;
         
         if(one == 0){
             return 0;
         }
+    
+        int zero = 0;
+        int min = Integer.MAX_VALUE;
         
         int j = 0;
         
-        for(int i=0;i<arr.length;++i){
-            
+        for(int i=0;i<arr.length;++i){  
             if(i>=one){
                 min = Math.min(min,zero);
                 if(arr[j++]==0){
                     zero--;
                 }
             }
-            
             if(arr[i]==0){
                 zero++;
             }
