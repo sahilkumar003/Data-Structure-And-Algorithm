7ad19fa264b2bc07d8322573b9b265e30dccbde6
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-02-21 11:21:54 +0530

    Time: 1 ms (87.19%), Space: 39.3 MB (83.76%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,27 +1,53 @@
 class Solution {
-    public boolean isHappy(int n) {
-        int sum = 0;
-        ArrayList<Integer> list = new ArrayList<>(); 
+     public int findSquare(int number){
+        int ans = 0;
         
-        while(n>0){
-            int last = n%10;
-            sum = sum + (last*last);
+        while(number>0){
+            int rem = number%10;
+            ans += rem*rem;
+            number /= 10;
+        }
+        
+        return ans;
+    }
+    
+    public boolean isHappy(int n) {
+//         int sum = 0;
+//         ArrayList<Integer> list = new ArrayList<>(); 
+        
+//         while(n>0){
+//             int last = n%10;
+//             sum = sum + (last*last);
          
-            n = n/10;
-            if(n==0){
-                 if(sum == 1){
+//             n = n/10;
+//             if(n==0){
+//                  if(sum == 1){
+//                 return true;
+//             }
+//                 n = sum;
+                
+//                 if(list.contains(sum)){
+//                     return false;
+//                 }else{
+//                     list.add(sum);
+//                 }
+//                 sum = 0;
+//             }
+//         }
+//         return false;
+        
+        int slow = n;
+         int fast = n;
+         
+         do{
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+            
+            if(slow==1){
                 return true;
             }
-                n = sum;
-                
-                if(list.contains(sum)){
-                    return false;
-                }else{
-                    list.add(sum);
-                }
-                sum = 0;
-            }
-        }
-        return false;
+         }while(slow!=fast);
+         
+         return false;
     }
 }
