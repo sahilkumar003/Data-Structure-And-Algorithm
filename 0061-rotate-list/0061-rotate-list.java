fd4a1a7590aedf324fc2f1d70b1c4aee0446f0df
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-02-23 11:07:18 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (5.14%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -9,34 +9,68 @@
  * }
  */
 class Solution {
+    public ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        
+        while(curr!=null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+    
     public ListNode rotateRight(ListNode head, int k) {
         if(head==null || head.next==null){
             return head;
         }
         
-        int size = 0;
-        ListNode temp = head;
+//         int size = 0;
+//         ListNode temp = head;
         
-        while(temp!=null){
+//         while(temp!=null){
+//             size++;
+//             temp = temp.next;
+//         }
+        
+//         k = k%size;
+        
+//         for(int i=1;i<=k;++i){
+//             ListNode slow = head;
+//             ListNode fast = head.next;
+//             while(fast.next!=null){
+//                 slow = slow.next;
+//                 fast = fast.next;
+//             }
+//             ListNode node = new ListNode(fast.val);
+//             slow.next = null;
+//             node.next = head;
+//             head = node;
+//         }
+        
+//         return head;
+        
+        head = reverse(head);
+        ListNode tail = head;
+        int size = 1;
+        
+        while(tail.next!=null){
+            tail = tail.next;
             size++;
-            temp = temp.next;
         }
         
         k = k%size;
         
-        for(int i=1;i<=k;++i){
-            ListNode slow = head;
-            ListNode fast = head.next;
-            while(fast.next!=null){
-                slow = slow.next;
-                fast = fast.next;
-            }
-            ListNode node = new ListNode(fast.val);
-            slow.next = null;
-            node.next = head;
-            head = node;
+        while(k-->0){
+            ListNode temp = new ListNode(head.val);
+            head = head.next;
+            tail.next = temp;
+            tail = temp;
         }
         
+        head = reverse(head);
         return head;
     }
 }
