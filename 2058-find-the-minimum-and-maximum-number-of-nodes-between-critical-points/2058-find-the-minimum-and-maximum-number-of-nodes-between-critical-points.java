b28b86298535f24c48e14e3246957976ebd9d285
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-02-25 11:21:46 +0530

    Time: 5 ms (93.00%), Space: 67.7 MB (18.93%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -14,33 +14,57 @@ class Solution {
             return new int[]{-1,-1};
         }
         
-        ArrayList<Integer> list = new ArrayList<>();
-        int k = 0;
-        ListNode curr = head.next;
-        ListNode prev = head;
+//         ArrayList<Integer> list = new ArrayList<>();
+//         int k = 0;
+//         ListNode curr = head.next;
+//         ListNode prev = head;
         
-        while(curr!=null && curr.next!=null){
-            ++k;
+//         while(curr!=null && curr.next!=null){
+//             ++k;
+//             ListNode temp = curr.next;
+//             if((curr.val>temp.val && curr.val>prev.val) || (curr.val<temp.val && curr.val<prev.val)){
+//                 list.add(k);
+//             }
+//             prev = curr;
+//             curr = temp;
+//         }
+        
+//         if(list.size()<=1){
+//             return new int[]{-1,-1};
+//         }
+        
+//         int min = Integer.MAX_VALUE;
+//         int max = Integer.MIN_VALUE;
+        
+//         for(int i=1;i<list.size();++i){
+//             min = Math.min(min,list.get(i)-list.get(i-1));
+//             max = Math.max(max,list.get(i)-list.get(0));
+//         }
+        
+//         return new int[]{min,max};
+        
+        ListNode prev = head;
+        ListNode curr = head.next;
+        int min = Integer.MAX_VALUE;
+        int last = -1;
+        int first = Integer.MAX_VALUE;
+        
+        for(int i=0;curr!=null && curr.next!=null;++i){
             ListNode temp = curr.next;
-            if((curr.val>temp.val && curr.val>prev.val) || (curr.val<temp.val && curr.val<prev.val)){
-                list.add(k);
+             if((curr.val>temp.val && curr.val>prev.val) || (curr.val<temp.val && curr.val<prev.val)){
+                if(last!=-1){
+                    min = Math.min(min,i-last);
+                }
+                 first = Math.min(first,i);
+                 last = i;
             }
             prev = curr;
             curr = temp;
         }
         
-        if(list.size()<=1){
+        if(min==Integer.MAX_VALUE){
             return new int[]{-1,-1};
         }
-        
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        
-        for(int i=1;i<list.size();++i){
-            min = Math.min(min,list.get(i)-list.get(i-1));
-            max = Math.max(max,list.get(i)-list.get(0));
-        }
-        
-        return new int[]{min,max};
+        return new int[]{min,last-first};
     }
 }
