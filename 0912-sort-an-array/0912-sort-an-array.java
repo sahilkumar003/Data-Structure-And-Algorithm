f16a7ab486e1eda23ab30ce0e15419754fc33b03
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-03-01 23:28:42 +0530

    Time: 101 ms (27.23%), Space: 52 MB (79.08%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0;i<nums.length;++i){
+            pq.add(nums[i]);
+        }
+        
+        for(int i=0;i<nums.length;++i){
+            nums[i] = pq.remove();
+        }
+        
+        return nums;
+    }
+}
