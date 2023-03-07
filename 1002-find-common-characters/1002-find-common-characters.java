cdf67b09fc066ff8e381be435d248a26f4989dbe
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-03-07 10:27:18 +0530

    Time: 3 ms (88.68%), Space: 42.9 MB (34.39%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,31 +1,29 @@
 class Solution {
     public List<String> commonChars(String[] words) {
+        ArrayList<String> list = new ArrayList<>();
+        int []minFreq = new int[26];
+        Arrays.fill(minFreq,Integer.MAX_VALUE);
         
-        ArrayList<String> ans = new ArrayList<>();
-        
-        int[] count = new int[26];
-        Arrays.fill(count,Integer.MAX_VALUE);
-        
-        for(String s : words){
-            int []arr = new int[26];
-            for(char c : s.toCharArray()){
-                arr[c-'a']++;
+        for(int i=0;i<words.length;++i){
+            int []currCharFreq = new int[26];
+            for(char curr : words[i].toCharArray()){
+                currCharFreq[curr - 'a']++;
             }
             
-            for(int i=0;i<26;++i){
-                count[i] = Math.min(count[i],arr[i]);
+            for(int j=0;j<26;++j){
+                minFreq[j] = Math.min(minFreq[j],currCharFreq[j]);
             }
         }
         
         for(int i=0;i<26;++i){
-            if(count[i]>0){
-                while(count[i]>0){
-                    ans.add(((char)(i + 'a')) + "");
-                    count[i]--;
+            if(minFreq[i]>0){
+                while(minFreq[i]>0){
+                 list.add((char)(i+'a')+"");   
+                  minFreq[i]--;  
                 }
             }
         }
         
-        return ans;
+        return list;
     }
 }
