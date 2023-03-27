ff39dd7601e9b130b1ddce52a264b2ca470beafb
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-03-27 17:34:42 +0530

    Time: 1 ms (100.00%), Space: 40.1 MB (28.03%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,7 +2,6 @@ class Solution {
     public int mySqrt(int x) {
         long start = 0;
         long end = x;
-        long ans = 0;
         
         while(start<=end){
             long mid = start + (end-start)/2;
@@ -10,7 +9,6 @@ class Solution {
             if((mid*mid)==x){
                 return (int)mid;
             }else if((mid*mid)<x){
-                ans = start;
                 start = mid+1;
             }else{
                 end = mid-1;
