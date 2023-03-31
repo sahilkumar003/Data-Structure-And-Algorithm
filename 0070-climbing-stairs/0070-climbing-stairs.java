66574b5ecaf64fd7b74cf1e530480a7054602cfe
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-03-31 23:30:13 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (26.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,27 @@
 class Solution {
-     int dp[]=new int[46];
-    public int climbStairs(int n) {
-        if(n==0){
-            return 0;
-        }if(n==1){
+    private int totalWays(int currStair, int target, Map<Integer,Integer> memo){
+        if(currStair==target){
             return 1;
         }
-        if(n==2){
-            return 2;
-        }
-        if(dp[n]!=0){
-            return dp[n];
+        if(currStair>target){
+            return 0;
         }
         
-     return  dp[n]=climbStairs(n-1) + climbStairs(n-2);
+        int currStairKey = currStair;
+        
+        if(memo.containsKey(currStairKey)){
+            return memo.get(currStairKey);
+        }
+        
+        int oneSteps = totalWays(currStair+1,target,memo);
+        int twoSteps = totalWays(currStair+2,target,memo);
+        
+        memo.put(currStairKey,oneSteps+twoSteps);
+        
+        return oneSteps+twoSteps;
+    }
+    
+    public int climbStairs(int n) {
+        return totalWays(0,n,new HashMap<Integer,Integer>());
     }
 }
