e5ebdf7e52cf90f3f83b404f59c34720617d03d5
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-04-03 16:16:14 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (55.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,25 @@
 class Solution {
-    public int fib(int n) {
+    private int ans(int n,Map<Integer,Integer> cache){
         if(n<2){
             return n;
         }
         
-        return fib(n-1) + fib(n-2);
+        int currKey = n;
+        
+        if(cache.containsKey(currKey)){
+            return cache.get(currKey);
+        }
+        
+        int firstHalf = ans(n-1,cache);
+        int secondHalf = ans(n-2,cache);
+        
+        cache.put(currKey,firstHalf+secondHalf);
+        
+        return cache.get(currKey);
+    }
+    
+    public int fib(int n) {
+        Map<Integer,Integer> cache = new HashMap<Integer,Integer>();
+       return ans(n,cache);
     }
 }
