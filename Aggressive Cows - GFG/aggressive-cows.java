4734b3964a059b1d4f101f05f2d5cb1518d37316
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-04-09 18:11:35 +0530

    Added solution - LeetHub

---
+++ Aggressive Cows - GFG/aggressive-cows.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+
+            int n;
+            n = sc.nextInt();
+
+            int k;
+            k = sc.nextInt();
+
+            int[] v = new int[n];
+            for (int i = 0; i < n; i++) v[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            int res = obj.solve(n, k, v);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int solve(int n, int k, int[] arr) {
+        int start = 1;
+        int end = 1000000000;
+        int ans = 0;
+        
+        Arrays.sort(arr);
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            int cow = 1;
+            int lastPosition = 0;
+            
+            for(int i=1;i<arr.length;++i){
+                if((arr[i]-arr[lastPosition])>=mid){
+                    cow++;
+                    lastPosition = i;
+                }
+            }
+            
+            if(cow<k){
+                end = mid-1;
+            }else{
+                ans = mid;
+                start = mid+1;
+            }
+        }
+        
+        return ans;
+    }
+}
