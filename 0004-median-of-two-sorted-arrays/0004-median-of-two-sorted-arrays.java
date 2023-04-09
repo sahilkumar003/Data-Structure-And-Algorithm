d1282480c8e2bc9e04df8e02cb75a432ae741985
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-04-09 17:21:54 +0530

    Time: 2 ms (100.00%), Space: 43.8 MB (25.19%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,38 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-         if(arr2.length<arr1.length){
-            return findMedianSortedArrays(arr2,arr1);
+        int []ans = new int[arr1.length + arr2.length];
+        
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i] < arr2[j]){
+                ans[k++] = arr1[i];
+                i++;
+            }else{
+                ans[k++] = arr2[j];
+                j++;
+            }
         }
         
-        int n1 = arr1.length;
-        int n2 = arr2.length;
-
-        int start = 0;
-        int end = arr1.length;
-
-        while(start<=end){
-            int cut1 = start + (end-start)/2;
-            int cut2 = (n1+n2+1)/2-cut1;
-
-            int l1 = cut1 == 0 ? Integer.MIN_VALUE : arr1[cut1-1];
-            int l2 = cut2 == 0 ? Integer.MIN_VALUE : arr2[cut2-1];
-            int r1 =  cut1 == n1 ? Integer.MAX_VALUE : arr1[cut1];
-            int r2 =  cut2 == n2 ?  Integer.MAX_VALUE : arr2[cut2];
-
-            while(l1<=r2 && l2<=r1){
-                if((n1+n2)%2==0){
-                    return (double)(Math.max(l1,l2)+Math.min(r1,r2))/2;
-                }else{
-                    return (double)(Math.max(l1,l2));
-                }
-            }
-
-            if(l1>r2){
-                end = cut1-1;
-            }else{
-                start = cut1+1;
-            }
+        while(i<arr1.length){
+            ans[k++] = arr1[i++];
         }
-        return 0.0;
+        
+        while(j<arr2.length){
+            ans[k++] = arr2[j++];
+        }
+        
+        if(ans.length%2 == 0){
+            return (double)((double)ans[ans.length/2] + (double)ans[ans.length/2 - 1]) / 2;
+        }else{
+            return (double)ans[ans.length/2];
+        }
+        
     }
 }
