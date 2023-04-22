095ce61ad4af807adbce4971a7eeaaad44121fcf
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-04-22 23:01:26 +0530

    Time: 2 ms (99.68%), Space: 60.3 MB (25.46%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,20 +1,28 @@
 class Solution {
     public int canCompleteCircuit(int[] petrol, int[] distance) {
+       int []arr = new int[petrol.length];
+            
+       for(int i=0;i<arr.length;++i){
+           arr[i] = petrol[i] - distance[i];
+       }
         int deficit = 0;
-        int balance = 0;
-        int start = 0;
+        int sum = 0;
+        int index = 0;
         
-        for(int i=0;i<petrol.length;++i){
-            balance += petrol[i] - distance[i];
-            if(balance<0){
-                deficit += balance;
-                balance = 0;
-                start = i+1;
+        for(int i=0;i<arr.length;++i){
+            sum += arr[i];
+            if(sum<0){
+                deficit += sum;
+                sum = 0;
+                index = i+1;
             }
         }
-        if((balance+deficit)>=0){
-            return start;
+        
+        if((sum+deficit)>=0){
+            return index;
         }
+        
+        
         return -1;
     }
 }
