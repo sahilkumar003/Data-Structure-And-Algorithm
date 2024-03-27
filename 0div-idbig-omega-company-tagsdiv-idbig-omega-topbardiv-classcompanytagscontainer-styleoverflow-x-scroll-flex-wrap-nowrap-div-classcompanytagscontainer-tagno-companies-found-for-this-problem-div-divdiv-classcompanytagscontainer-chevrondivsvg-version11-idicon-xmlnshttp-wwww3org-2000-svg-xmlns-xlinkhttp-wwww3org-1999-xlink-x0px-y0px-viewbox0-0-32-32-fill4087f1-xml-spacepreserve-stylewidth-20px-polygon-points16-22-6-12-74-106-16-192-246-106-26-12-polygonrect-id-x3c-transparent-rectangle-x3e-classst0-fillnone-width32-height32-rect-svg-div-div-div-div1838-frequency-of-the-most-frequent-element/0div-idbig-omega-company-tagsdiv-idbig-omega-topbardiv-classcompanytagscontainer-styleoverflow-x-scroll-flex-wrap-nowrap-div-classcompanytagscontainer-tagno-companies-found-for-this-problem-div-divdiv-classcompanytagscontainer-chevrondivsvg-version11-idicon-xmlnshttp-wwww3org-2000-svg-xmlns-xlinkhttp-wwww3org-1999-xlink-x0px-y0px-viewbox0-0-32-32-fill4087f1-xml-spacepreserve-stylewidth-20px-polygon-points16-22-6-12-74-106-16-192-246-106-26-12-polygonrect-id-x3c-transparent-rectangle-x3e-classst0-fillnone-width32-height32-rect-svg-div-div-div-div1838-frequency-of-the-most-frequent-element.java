ce7c2e08842b6f0c9dba7d42066d734394e9ba51
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2024-03-27 12:29:10 +0530

    Time: 29 ms (90.80%), Space: 56.4 MB (71.91%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element.java
@@ -1,43 +1,22 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-//         Arrays.sort(nums);
+        Arrays.sort(nums);
         
-//         int leftPointer = 0;
-//         int maximumPossibleFrequency = 1;
-//         long windowSum = 0;
+        int leftPointer = 0;
+        int maximumPossibleFrequency = 1;
+        long windowSum = 0;
         
-//         for(int rightPointer=0;rightPointer<nums.length;rightPointer++){
-//             windowSum += nums[rightPointer];
+        for(int rightPointer=0;rightPointer<nums.length;rightPointer++){
+            windowSum += nums[rightPointer];
             
-//             while((long )(nums[rightPointer] * (rightPointer - leftPointer + 1)) > (windowSum + k)){
-//                 windowSum -= nums[leftPointer];
-//                 leftPointer++;
-//             }
-            
-//             maximumPossibleFrequency = Math.max(maximumPossibleFrequency, rightPointer - leftPointer + 1);
-//         }
-        
-//         return maximumPossibleFrequency;
-        
-        
-         int maxFrequency = 0; // Initialize the maximum frequency
-        long currentSum = 0; // Initialize the current sum of the subarray elements
-
-        Arrays.sort(nums); // Sort the array in ascending order
-
-        for (int left = 0, right = 0; right < nums.length; ++right) {
-            currentSum += nums[right]; // Include the current element in the subarray sum
-
-            // Check if the current subarray violates the condition (sum + k < nums[right] * length)
-            while (currentSum + k < (long) nums[right] * (right - left + 1)) {
-                currentSum -= nums[left]; // Adjust the subarray sum by removing the leftmost element
-                left++; // Move the left pointer to the right
+            while((long) nums[rightPointer] * (rightPointer - leftPointer + 1) > windowSum + k){
+                windowSum -= nums[leftPointer];
+                leftPointer++;
             }
-
-            // Update the maximum frequency based on the current subarray length
-            maxFrequency = Math.max(maxFrequency, right - left + 1);
+            
+            maximumPossibleFrequency = Math.max(maximumPossibleFrequency, rightPointer - leftPointer + 1);
         }
-
-        return maxFrequency;
+        
+        return maximumPossibleFrequency;
     }
 }
