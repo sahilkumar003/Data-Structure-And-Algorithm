da9b11407052324cd12c3cd6f27c73acd1bb972c
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2024-07-23 07:14:25 +0530

    Time: 486 ms (5.05%), Space: 43.4 MB (22.74%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,24 @@
+class Solution:
+    def bestTimeToBuyStock(self, prices, currentIndex, canBuy, numberOfTransactionAllowed, memo):
+        if numberOfTransactionAllowed == 0 or currentIndex == len(prices):
+            return 0;
+        
+        currentStockPrice = prices[currentIndex];
+        currentKey = str(currentIndex) + "-" + str(canBuy) + "-" + str(numberOfTransactionAllowed);
+        
+        if currentKey in memo:
+            return memo[currentKey];
+        
+        if canBuy == 1:
+            idle = self.bestTimeToBuyStock(prices, currentIndex+1, canBuy, numberOfTransactionAllowed, memo);
+            buy = - currentStockPrice + self.bestTimeToBuyStock(prices, currentIndex+1, 0, numberOfTransactionAllowed, memo);
+            memo[currentKey] = max(idle, buy);
+        else:
+            idle = self.bestTimeToBuyStock(prices, currentIndex+1, canBuy, numberOfTransactionAllowed, memo);
+            sell = currentStockPrice + self.bestTimeToBuyStock(prices, currentIndex+1, 1, numberOfTransactionAllowed-1, memo);
+            memo[currentKey] = max(idle,sell);
+        return memo[currentKey];
+    
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+         return self.bestTimeToBuyStock(prices, 0, 1, k, {});
+        
