30d15a36c2ace10ca06c0f5c546b820c9727e63e
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2024-08-16 14:41:03 +0530

    Time: 218 ms (5.14%), Space: 18.2 MB (24.13%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findPath(self, currentRow, currentCol, matrix, memo):
+        if (currentRow > len(matrix)-1 or currentCol < 0 or currentCol > len(matrix[0])-1):
+            return 10000;
+        
+        if (currentRow == len(matrix)-1):
+            return matrix[currentRow][currentCol];
+        
+        currentValue = matrix[currentRow][currentCol];
+        currentKey = str(currentRow) + "-" + str(currentCol);
+        
+        if currentKey in memo:
+            return memo[currentKey];
+        
+        leftDiagonal = currentValue + self.findPath(currentRow+1, currentCol-1, matrix, memo);
+        down = currentValue + self.findPath(currentRow+1, currentCol, matrix, memo);
+        rightDiagonal = currentValue + self.findPath(currentRow+1, currentCol+1, matrix, memo);
+        
+        memo[currentKey] = min(min(leftDiagonal, down), rightDiagonal);
+            
+        return min(min(leftDiagonal, down), rightDiagonal);
+            
+    
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        minPath = 10000;
+        memo = {}
+        for i in range(len(matrix)):
+            temp = self.findPath(0, i, matrix, memo);
+            minPath = min(minPath, temp);
+            
+        return minPath;
+        
