220ba3e3f3501efcc675191f44f53544e89d7e13
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2024-08-21 09:08:49 +0530

    Time: 1836 ms (5.04%), Space: 139.2 MB (17.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -3,22 +3,21 @@ class Solution:
         if index1 >= len(str1) or index2 >= len(str2):
             return 0;
         
-        moveBothIndex = 0;
-        moveOneIndex = 0;
+        ans = 0;
         currentKey = str(index1) + "-" + str(index2);
         
         if currentKey in memo:
             return memo[currentKey];
         
         if str1[index1] == str2[index2]:
-            moveBothIndex = 1 + self.findLCS(index1+1, index2+1, str1, str2, memo)
+            ans = 1 + self.findLCS(index1+1, index2+1, str1, str2, memo)
         else:
             moveFirstIndex = self.findLCS(index1+1, index2, str1, str2, memo)
             moveSecondIndex = self.findLCS(index1, index2+1, str1, str2, memo)
-            moveOneIndex= max(moveFirstIndex, moveSecondIndex)
+            ans= max(moveFirstIndex, moveSecondIndex)
         
-        memo[currentKey] = max(moveBothIndex, moveOneIndex)
-        return max(moveBothIndex, moveOneIndex);    
+        memo[currentKey] = ans
+        return ans;    
             
     
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
