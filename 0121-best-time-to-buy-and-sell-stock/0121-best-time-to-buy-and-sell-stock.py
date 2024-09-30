bf43ef73e3b9564f5b74bf369891afd582a23001
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2024-09-30 09:31:59 +0530

    Time: 2157 ms (5.00%), Space: 76 MB (5.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,22 +4,23 @@ class Solution:
             return 0;
         
         currentStockPrice = prices[currentIndex];
-        buy = -10000;
-        sell = -10000;
-        
         currentKey = str(currentIndex) + "-" + str(canBuy) + "-" + str(numberOfTransactionAllowed);
         
         if currentKey in memo:
             return memo[currentKey];
         
-        idle = self.bestTimeToBuyStock(prices, currentIndex+1, canBuy, numberOfTransactionAllowed, memo);
-        if canBuy == 1:  
+        buy = 0;
+        sell = 0
+        
+        if canBuy == 1:
             buy = - currentStockPrice + self.bestTimeToBuyStock(prices, currentIndex+1, 0, numberOfTransactionAllowed, memo);
         else:
             sell = currentStockPrice + self.bestTimeToBuyStock(prices, currentIndex+1, 1, numberOfTransactionAllowed-1, memo);
+            
+        idle = self.bestTimeToBuyStock(prices, currentIndex+1, canBuy, numberOfTransactionAllowed, memo);
+        memo[currentKey] = max(idle, buy, sell);
         
-        memo[currentKey] = max(idle, buy) if buy != -10000 else max(idle, sell);
-        return memo[currentKey];
+        return memo[currentKey];    
         
     
     def maxProfit(self, prices: List[int]) -> int:
